47e805ec5d460b2e11c39fb875b04a220aecb90e
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-04 14:47:36 -0400

    Complete clean up

--- cleanUp.js
+++ cleanUp.js
@@ -3,27 +3,28 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function nameQuestion() {
+function askName() {
   console.log("Hello, what is your name?")
 }
 
-nameQuestion()
+askName()
+//change the name of the function
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+sum = first + second + third
 console.log(sum);
       }
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+//didn't to define sum as a variable.
 
 // EX 3:
 func makeFreshPesto(){
@@ -31,16 +32,22 @@ func makeFreshPesto(){
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
+//closing curly bracket could be moved down a line
+
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
-    var avg = sum / 2;
+var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
+  console.log(`the average is: ${avg}.`)
   }
+average(3,9)
+
+//put the first curly bracket for the function on the first line. Call the function at the end.
+//put the period inside the interpolation backticks. put both sum and avg equations at the same indentation. 
